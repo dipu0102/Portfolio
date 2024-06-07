f9151ac76562079f8a60bbf26c9a84c33a7fb14f
dipankar <dipankargarai420@gmail.com>
2024-06-07 15:33:44 +0530

    experiance and about section added and fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import PortFolio from "./components/PortFolio";
 function App() {
   return (
     <>
       <div>
         <Navbar />
         <Home />
+        <About />
+        <PortFolio />
       </div>
     </>
   );
